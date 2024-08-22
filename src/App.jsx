d109f7371ed00e9d279673a3ada777e175ee0bc9
mauroSoliz98 <maurososoliz@gmail.com>
2024-08-22 14:11:56 -0400

    Se aplico json-server exitosamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Filter } from './components/Filter'
 import { PersonForm } from './components/PersonForm'
 import { Persons } from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')  // Declara una variable de estado...
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('') //creamos un variable useState buleana
+
+  useEffect(() => {
+    const loadPersons = async() => {
+      const showPersons = await axios.get("http://localhost:3004/persons")
+      const response = showPersons.data
+      setPersons(response)
+    }
+    loadPersons()
+  }, [])
+  
 
   //ACTUALIZA EL ESTADO DEL INPUT
   const handleInputName = (event) => {
